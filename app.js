f2a18acdfa85dc8266c08a94d9b0a0eed406581a
Sümeyra Kılıçoğlu <119740024+Smyakilicoglu@users.noreply.github.com>
2023-08-31 01:16:10 +0300

    Level 5 - Cookie and Session

--- app.js
+++ app.js
@@ -3,29 +3,39 @@ import ejs from "ejs";
 import 'dotenv/config'
 import mongoose from "mongoose";
 import express from "express";
-import bodyParser from "body-parser"
-import bcrypt from "bcrypt";
-const saltRounds = 10;
-//buradaki sayı ne kadar artarsa bilgisayar o kadar karmaşık sayı üretmek için çalışır.
-//Sayı ne kadar artarsa oluyşturulmak için verilen sürenin artması gerekir.
+import bodyParser from "body-parser";
+import session from "express-session";
+import passport from "passport";
+import passportLocalMongoose from "passport-local-mongoose";
+//Daha az kod satırıyla daha çok iş yapmak için kullanılır passport-local-mongoose
 
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+//Uygulamada gerekli oturum paketi için bu kodlar gerekli
+app.use(session({
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized: false,
+}));
+
+//Oturumun başlaması için passport kullanılmasını ve passport paketinin başlatılması için gerekli olan kod.
+app.use(passport.initialize());
+app.use(passport.session());
+// Oturumu başlatmamız için yapmamız gerekenler.
 
 mongoose.connect("mongodb://127.0.0.1:27017/SecretsDB");
 
 const UserSchema = new mongoose.Schema({
-    email: {
-        type: String,
-        trim: true, 
-        lowercase: true,
-        required: 'Email address is required',
-    }
-    ,
-    password: {
-        required: true,
-        type: String,
-    },
+    email: String,
+    password: String,
 })
 
+UserSchema.plugin(passportLocalMongoose);
 
 
 const SecretShema = new mongoose.Schema({
@@ -35,64 +45,87 @@ const SecretShema = new mongoose.Schema({
 const Secret = mongoose.model("Secret", SecretShema);
 const User = mongoose.model("User", UserSchema);
 
+passport.use(User.createStrategy());
 
-const app = express();
-const port = 3000;
-
-app.set('view engine', 'ejs');
-
-app.use(bodyParser.urlencoded({extended: true}));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", (req,res) => {
     res.render("home");
 })
+
+app.get("/logout", (req, res) => {
+    req.logout(req.user, err => {
+      if(err) return next(err);
+      res.redirect("/");
+    });
+});
 
 app.get("/login", (req,res)=> {
     res.render("login");
 })
 
 app.post("/login", (req,res)=> {
-    const username = req.body.username;
-    const password = req.body.password;
-    User.findOne({email: username}).then(function(foundUser){
-        bcrypt.compare(password, foundUser.password , function(err, result) {
-            // result == true hash = faundUser.password 
-            if (result === true) {
-                res.render("secrets")
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+      });
+    //Eğer burada oluşan bir hata varsa consola hatayı kaydeder yoksa şifreyi doğrulayıp sırrın bulunduğu sayfaya geçer.
+    //register bölüminde kaydeilen çerezin burada kullanılma vakti.
+      req.login(user, function(err){
+        if (err) {
+          console.log(err);
+        } else {
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/secrets");
+              });
             }
-        })
-    }) .catch (function(err){
-        console.log(err)
-    })
-    
+    });
+        
 });
+
 
 app.get("/register", (req,res)=> {
     res.render("register")
 })
 
+app.get("/secrets", (req,res) => {
+    //Tekrar giriş yapılmak istenildiğinde eğer oturum açıldıysa gizli sayfaya eğer oturum açılmadıysa register'a yönlendirir.
+    if (req.isAuthenticated()) {
+        res.render("secrets");
+    } else {
+        // eğer arma çubuğun https://localhost:3000/secrets yazarak sırların bulunduğu sayfaya gitmek istenilirse 
+        //login sayfasına yönlendirilecek.
+        res.redirect("/login");
+    }
+})
+
 app.post("/register", (req,res)=> {
 
-    bcrypt.genSalt(saltRounds, function(err, salt) {
-        bcrypt.hash(req.body.password, salt, function(err, hash) {
-            // Store hash in your password DB.
-            const newUser = new User({
-                email: req.body.username,
-                password: hash
-            });
-            newUser.save().then(()=> {console.log("Kaydedildi."), res.render("secrets");}) .catch((err) => {console.log(err)});
+    User.register({username: req.body.username}, req.body.password , function(err, user) {
+        if (err) { 
+            console.log(err);
+            res.redirect("/register")
+         } 
+        /* 
+        const authenticate = User.authenticate();
+         authenticate(req.body.username, req.body.password, function(err, result) {
+            if (result === true) {
+                res.redirect("/secrets")
+            } else {
+                console.log(err)
+            }
         });
-    });
-
-    
-    /*
-    User.insertMany(emailarray).then(function(){
-        emailarray.push(mail);  
-    }) .catch(function(err){
-        console.log(err);
-    }) */
-    
-})
+    });*/
+        
+        else {
+            //mail ve şifre oluşturulduktan sonra kaydedilmesi ve gizli sayfaya gidilmesi gerekir.
+            passport.authenticate("local")(req, res, function() {
+                res.redirect("/secrets")
+            })
+         }
+        }); 
+});
 
 app.get("/submit", (req,res)=> {
     res.render("submit");
